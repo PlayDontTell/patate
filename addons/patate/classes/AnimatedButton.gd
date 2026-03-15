cdbf5b7a651562e01bd4900efbeff3171baa4525
IvanVoirol <ivan@voirol.org>
2026-03-15 12:46:40 +0100

    feat(AnimatedButton): simplified its config, and improved animation to better fit different buttons sizes with same config

--- addons/patate/classes/AnimatedButton.gd
+++ addons/patate/classes/AnimatedButton.gd
@@ -24,20 +24,28 @@ func _center_pivot() -> void:
 func _on_focused() -> void:
 	if not config or disabled:
 		return
-	_animate(config.focus_scale, config.focus_duration, config.focus_ease, config.focus_trans)
+	_animate(config.focus_margins, config.focus_max_scale, config.focus_duration, config.focus_ease, config.focus_trans)
 
 
 func _on_unfocused() -> void:
 	if not config or disabled:
 		return
-	_animate(config.unfocus_scale, config.unfocus_duration, config.unfocus_ease, config.unfocus_trans)
+	_animate(0., 1., config.unfocus_duration, config.unfocus_ease, config.unfocus_trans)
 
 
-func _animate(target_scale : Vector2, duration : float, ease_type : Tween.EaseType, trans_type : Tween.TransitionType) -> void:
+func _animate(margins : float, max_scale : float, duration : float, ease_type : Tween.EaseType, trans_type : Tween.TransitionType) -> void:
 	if is_instance_valid(_tween):
 		_tween.kill()
 	
+	var new_scale : Vector2 = Vector2.ONE * min(
+		(self.size.x + margins * 2.) / self.size.x,
+		max_scale,
+	)
+	
 	_tween = create_tween()
-	_tween.tween_property(self, "scale", target_scale, duration) \
-		.set_ease(ease_type) \
-		.set_trans(trans_type)
+	_tween.tween_property(
+		self,
+		"scale",
+		new_scale,
+		duration
+	).set_ease(ease_type).set_trans(trans_type)
--- src/core/core_scenes/main_menu/settings_menu/binding_menus/binding_panel.gd
+++ src/core/core_scenes/main_menu/settings_menu/binding_menus/binding_panel.gd
@@ -9,6 +9,10 @@ func _ready() -> void:
 	update()
 
 
+func _input(event: InputEvent) -> void:
+	pass
+
+
 func update() -> void:
 	for binding_row in self.get_children():
 		binding_row.queue_free()
@@ -21,4 +25,15 @@ func update() -> void:
 		var new_binding_row : Control = BINDING_ROW.instantiate()
 		new_binding_row.action = action
 		new_binding_row.device_filter = device_filter
+		new_binding_row.rebind_requested.connect(_on_rebind_requested)
+		new_binding_row.reset_requested.connect(_on_reset_requested)
+
 		self.add_child(new_binding_row)
+
+
+func _on_rebind_requested(action: StringName) -> void:
+	pass
+
+
+func _on_reset_requested(action: StringName) -> void:
+	pass
